b8ab4a8c0655a8cfbd9f7da61506d92ce2fd37c2
Simon Micheneau <contact@simon-micheneau.fr>
2021-10-28 10:40:04 +0200

    feat: Update screen name for game server

--- src/Service/GameServerOperations.php
+++ src/Service/GameServerOperations.php
@@ -8,6 +8,6 @@ class GameServerOperations
 {
     public static function getGameServerNameScreen(GameServer $game): string
     {
-        return strtolower(str_replace(' ', '', $game->getName())).'_'.$game->getId();
+        return 'gameserver_'.$game->getId();
     }
 }
