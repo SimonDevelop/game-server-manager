335d8daf1027ad19c9d42a80410c9d6e7a886dd3
Simon Micheneau <contact@simon-micheneau.fr>
2022-07-28 15:48:24 +0200

    feat: Add cron command for state game servers check

---
+++ src/Command/CronServerCheckCommand.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Command;
+
+use App\Repository\GameServerRepository;
+use App\Service\GameServerOperations;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Service\Connection;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'cron:server:check',
+    description: 'Checks the status of the game servers and updates them if necessary.',
+)]
+class CronServerCheckCommand extends Command
+{
+    #@var GameServerRepository
+    private $gameServerRepository;
+
+    #@var GameServerOperations
+    private $gameOperations;
+
+    #@var EntityManagerInterface
+    private $em;
+
+    #@var Connection
+    private $connection;
+
+    #@param GameServerRepository
+    #@param GameServerOperations
+    #@param EntityManagerInterface
+    #@param Connection
+    public function __construct(
+        GameServerRepository $gameServerRepository,
+        GameServerOperations $gameOperations,
+        EntityManagerInterface $em,
+        Connection $connection
+    )
+    {
+        $this->gameServerRepository = $gameServerRepository;
+        $this->gameOperations       = $gameOperations;
+        $this->em                   = $em;
+        $this->connection           = $connection;
+
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $gameServers = $this->gameServerRepository->findAll();
+        foreach ($gameServers as $gameServer) {
+            $connection = $this->connection->getConnection($gameServer->getServer());
+            if (null !== $connection) {
+                $state   = $gameServer->getState();
+                $name    = $this->gameOperations->getGameServerNameScreen($gameServer);
+                $command = "screen -ls | grep -i \"{$name}\"";
+                $logs    = $this->connection->sendCommandWithResponse($connection, $command);
+
+                if ($state === 'Off' && str_contains($logs, $name)) {
+                    $output->writeln("$name is running.");
+                    $gameServer->setStateType(1);
+                    $this->em->persist($gameServer);
+                    $this->em->flush();
+                }
+
+                if ($state === 'On' && !str_contains($logs, $name)) {
+                    $output->writeln("$name is not running.");
+                    $gameServer->setStateType(0);
+                    $this->em->persist($gameServer);
+                    $this->em->flush();
+                }
+            }
+        }
+
+        return Command::SUCCESS;
+    }
+}
